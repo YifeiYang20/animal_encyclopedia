268d18936593dbc35b1afbc14d33792d482ad8d9
YifeiYang20 <94498682+YifeiYang20@users.noreply.github.com>
2022-10-02 20:52:32 -0400

    10/02/2022 YYang

---
+++ src/main/java/com/animal_encyclopedia/animal_encyclopedia/AnimalEncyclopediaApplication.java
@@ -0,0 +1,13 @@
+package com.animal_encyclopedia.animal_encyclopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalEncyclopediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalEncyclopediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animal_encyclopedia/animal_encyclopedia/AnimalEncyclopediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animal_encyclopedia.animal_encyclopedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalEncyclopediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
